2e8dd517cb717c522615084b4ea35821a3594d51
Carlos Domínguez <kingio@yopmail.com>
2015-02-19 16:40:46 +0100

    Improved site

--- wuaki.js
+++ wuaki.js
@@ -1,5 +1,3 @@
-/* global $ */
-/* global document */
 module.exports = function() {
 
     var getMediaType = function() {
@@ -96,10 +94,46 @@ module.exports = function() {
         return media;
     };
 
-    var checkSelectors = {
-        '.interactions .favorite': 'pending', //WTF?
-        '.interactions .already_seen': 'watched',
-        '.snapshot-overlay[data-is-purchased=true] span': 'watched',
+    var checkSelectors = function() {
+        return {
+            '.interactions .favorite': {
+                manipulateData: function(data, $selector) {
+                    // Avoid if was already watched (else would be overwritten)
+                    if ($selector.parent().siblings('li').children('a').hasClass('active')) {
+                        data.sendStatus.abort = true;
+                        return false;
+                    }
+
+                    // Be aware: '.interactions .already_seen' selector uses same values
+                    data.sendStatus.value = ($selector.hasClass('active')) ? 'no_status' : 'pending';
+
+                    return data;
+                }
+            },
+
+
+            '.interactions .already_seen': {
+                manipulateData: function(data, $selector) {
+                    data.sendStatus.value = ($selector.hasClass('active')) ? 'no_status' : 'watched';
+
+                    // if "Quiero ver" is selected, dont mark as 'no_status', mark as pending
+                    if (data.sendStatus.value == 'no_status' && $selector.parent().siblings('li').children('a').hasClass('active')) {
+                        console.log('Pending will be set instead of no_status.');
+                        data.sendStatus.value = 'pending';
+                    }
+
+                    return data;
+                }
+            },
+
+            //'.interactions .favorite:not(".active")': 'pending', //WTF?
+            //'.interactions .favorite.active': 'no_status',
+
+            //'.interactions .already_seen:not(".active")': 'watched',
+            //'.interactions .already_seen.active': 'no_status',
+
+            '.snapshot-overlay[data-is-purchased=true] span': 'watched',
+        };
     };
 
     return {
