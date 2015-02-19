7c243ee8ec93b2bde75b6895840801af5e9fefb7
Carlos Domínguez <kingio@yopmail.com>
2015-02-19 16:36:57 +0100

    Improved matcher

--- atresplayer.js
+++ atresplayer.js
@@ -1,12 +1,10 @@
-/* global $ */
-/* global document */
 module.exports = function() {
 
     var getMediaType = function() {
 
         if (typeof(document) !== 'undefined') {
 
-            if (document.location.href.match(/\/television\/.*temporada-.*\/capitulo-/)) {
+            if (document.location.href.match(/\/television\/.*temporada-.*\/capitulo-/) || document.location.href.match(/\/programas\/.*\/temporada-\d+\/.+/)) {
                 return 'EPISODE';
             } else {
                 if (document.location.href.match(/\/series\//) || document.location.href.match(/\/programas\//)) {
@@ -29,6 +27,35 @@ module.exports = function() {
             directors: [],
             season: null,
             episode: null,
+            // Allows a custom parser
+            episodeParser: function(media, season) {
+
+                if (!isNaN(season)) {
+                    var i, j;
+
+                    var title = $('h3.mar-r_5').text().toLowerCase();
+
+                    for (i in media.seasons) {
+                        for (j in media.seasons[i]) {
+                            var episodeTitle = media.seasons[i][j].name.toLowerCase();
+                            var pieces, piece;
+
+                            // Example: El hormiguero | atresplayer: Jordi Évole | tviso: 5 de febrero de 2015 (Jordi Évole)
+                            pieces = episodeTitle.match(/.+\((.+)\)/);
+
+                            if (pieces != null && pieces[1] != null) {
+                                piece = pieces[1];
+                            }
+
+                            if (media.seasons[i][j].season === parseInt(season) && (episodeTitle === title || piece === title)) {
+                                return media.seasons[i][j];
+                            }
+                        }
+                    }
+                }
+
+                return false;
+            }
         },
         aux = null,
         i = 0;
@@ -43,8 +70,29 @@ module.exports = function() {
                 aux = $('.mar-r_5:nth-child(3) a').text().match(/T(\d+)/);
                 media.season = aux[1];
 
-                aux = $('.mar-r_5:nth-child(5)').text().match(/C(\d+)/);
-                media.episode = aux[1];
+                var title = $('h3.mar-r_5').text();
+
+                // Example: Velvet | Capitulo 1
+                var parsedEpisodeTitle = title.match(/Capítulo (\d+)/);
+
+                if (parsedEpisodeTitle != null) {
+                    media.episode = parseInt(parsedEpisodeTitle[1]);
+                }
+
+                // Example: Top chef | Programa 12
+                if (media.episode == null) {
+                    parsedEpisodeTitle = title.match(/Programa (\d+)/);
+
+                    if (parsedEpisodeTitle != null) {
+                        media.episode = parseInt(parsedEpisodeTitle[1]);
+                    }
+                }
+
+                if (media.episode == null) {
+                    aux = $('.mar-r_5:nth-child(5)').text().match(/C(\d+)/);
+                    media.episode = aux[1];
+                }
+
             } else {
                 //TITLE
                 media.title = $('h2[itemprop="name"]').text();
@@ -99,12 +147,17 @@ module.exports = function() {
             }
             
         }
-
+        console.log(media);
         return media;
     };
 
-    var checkSelectors = {
-        '#overview-bottom .wlb_classic_wrapper .btn2_text:first': 'pending',
+    var checkSelectors = function() {
+
+        return {
+            '#overview-bottom .wlb_classic_wrapper .btn2_text:first': 'pending',
+
+            '.mod_player .play': 'watched'
+        };
     };
 
     return {
